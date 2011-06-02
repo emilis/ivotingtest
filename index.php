6e86abaa286fbe7ff0fb5af3ffd5ff84b3049101
Emilis Dambauskas <emilis.d@gmail.com>
2011-06-02 03:48:35 +0300

    Improved first page and template.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require dirname(__FILE__) . "/system/lib/kernel.php";
 setOutputVar("title", "Sveiki!");
 
 ?>
-<div align="center">
+<div class="dialog">
     <p>Kviečiame Jus išbandyti internetinio balsavimo sistemą!</p>
     <p>Tokia (ar iš esmės panaši) sistema galėtų būti naudojama artimiausiuose Seimo, Prezidento ar savivaldos rinkimuose Lietuvoje.</p>
     <p>Tikimės, kad šis testas jums bus įdomus ir naudingas.</p>
--- system/lib/content-template.php
+++ system/lib/content-template.php
@@ -9,7 +9,10 @@
     <script type="text/javascript" src="<?php echo FILES_URL; ?>/ivotingtest.js"></script>
 </head>
 <body>
-    <div id="header">E-balsavimo testas 2012</div>
+    <div id="header">
+        <a href="index.php">E-balsavimo testas 2012</a>
+        <span class="credits">Sistemą kūrė: <em>UAB Elektroninių rinkimų prichvatizacija</em>, auditavo: <em>UAB Absoliučiai saugus skaitmeninis atkatas</em></span>
+    </div>
     <div id="content"><?php
 
         if (isset($title)) {
